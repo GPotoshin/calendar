bda14e3add502023b95d3e2be4f96590e3ee5a19
George Potoshin <george.potoshin@gmail.com>
2025-11-13 09:04:28 +0100

    multifile script

--- script/scrollable_calendar.js
+++ script/scrollable_calendar.js
@@ -1,51 +1,4 @@
-let centralBlockOffset = 0;
-const WEEKS_PER_BLOCK = 12;
-const CURRENT_WEEK_INDEX = 3;
-
-let app_state = {
-  selected_element: null,
-};
-
-function getPath(u) {
-  try {
-    const url_obj = new URL(u);
-    return url_obj.pathname;
-  } catch (e) {
-    console.error("Invalid URL:", u, e);
-    return "";
-  }
-}
-
-let sideMenuIsOpen = false;
-function handleClickOnSideMenuButton(button) {
-  if (sideMenuIsOpen) {
-    button.setAttribute('hx-target', '#side-menu');
-    button.removeAttribute('hx-get');
-    button.setAttribute('hx-swap', 'delete');
-  } else {
-    button.setAttribute('hx-get', '/api/side-menu');
-    button.setAttribute('hx-target', '#side-menu-container');
-    button.setAttribute('hx-swap', 'innerHTML');
-  }
-
-  sideMenuIsOpen ^= true;
-}
-
-function setMonthScrollPosition() {
-  const calendarBody = document.getElementById('calendar-body');
-  const calendarContent = document.getElementById('calendar-content');
-
-  const originalScrollBehavior = calendarBody.style.scrollBehavior;
-  calendarBody.style.scrollBehavior = 'auto';
-
-  const week = calendarContent.querySelectorAll('.week-row')[0];
-  calendarBody.scrollTop = week.offsetHeight*(6+3);
-  calendarBody.style.scrollBehavior = originalScrollBehavior;
-}
-
-let previousScroll = 0;
-
-let drag_state = {
+let state = {
   cells: null,
   backed_week: null,
   week: null,
@@ -59,31 +12,43 @@ let drag_state = {
   prev_focus_num: 0,
 };
 
-function getCellNum(state, pos_x) {
+export function setMonthScrollPosition() {
+  const calendarBody = document.getElementById('calendar-body');
+  const calendarContent = document.getElementById('calendar-content');
+
+  const originalScrollBehavior = calendarBody.style.scrollBehavior;
+  calendarBody.style.scrollBehavior = 'auto';
+
+  const week = calendarContent.querySelectorAll('.week-row')[0];
+  calendarBody.scrollTop = week.offsetHeight*(6+3);
+  calendarBody.style.scrollBehavior = originalScrollBehavior;
+}
+
+function getCellNum(pos_x) {
   week_rect = state.week.getBoundingClientRect(); 
 
   rel_pos_x = pos_x-week_rect.left;
   return Math.floor(rel_pos_x/(week_rect.width/7.0));
 }
 
-function handleMouseDown(e) {
+export function handleMouseDown(e) {
   if (e.button !== 0) return;
 
-  drag_state.isDragging = true;
-  drag_state.start_x = e.clientX;
-  drag_state.start_y = e.clientY;
+  state.isDragging = true;
+  state.start_x = e.clientX;
+  state.start_y = e.clientY;
 
   let cell = e.target.closest(".day-cell");
-  drag_state.week = cell.closest(".week-row");
-  drag_state.backed_week = drag_state.week.cloneNode(true);
-  drag_state.cells = drag_state.week.getElementsByClassName('day-cell');
-  drag_state.start_cell_num = getCellNum(drag_state, e.clientX);
-  drag_state.prev_focus_num = drag_state.start_cell_num;
+  state.week = cell.closest(".week-row");
+  state.backed_week = state.week.cloneNode(true);
+  state.cells = state.week.getElementsByClassName('day-cell');
+  state.start_cell_num = getCellNum(state, e.clientX);
+  state.prev_focus_num = state.start_cell_num;
 }
 
-function handleMouseUp(e) {
-  drag_state.isCreated = false;
-  drag_state.isDragging = false;
+export function handleMouseUp(e) {
+  state.isCreated = false;
+  state.isDragging = false;
 }
 
 function shiftBarBy(bar, n) {
@@ -148,109 +113,53 @@ function addBarToCell(state) {
 
 let event_counter = 1;
 
-function handleMouseMove(e) {
-  if (!drag_state.isDragging) return;
+export function handleMouseMove(e) {
+  if (!state.isDragging) return;
 
   const d = 50;
-  if ((drag_state.start_x-e.clientX>d || drag_state.start_x-e.clientX<-d)
-    && !drag_state.isCreated) {
+  if ((state.start_x-e.clientX>d || state.start_x-e.clientX<-d)
+    && !state.isCreated) {
 
-    drag_state.bar = document.createElement('div');
-    drag_state.bar.classList.add('event-occurence');
-    drag_state.bar.classList.add('event-single');
-    drag_state.bar.classList.add('no-select');
-    drag_state.bar.textContent = 'Event #'+event_counter;
+    state.bar = document.createElement('div');
+    state.bar.classList.add('event-occurence');
+    state.bar.classList.add('event-single');
+    state.bar.classList.add('no-select');
+    state.bar.textContent = 'Event #'+event_counter;
     event_counter+=1;
 
-    drag_state.bar.dataset.rightCellNum = drag_state.start_cell_num;
-    drag_state.bar.dataset.leftCellNum = drag_state.start_cell_num;
-    drag_state.bar.dataset.top = 0;
-    drag_state.bar.style.top = "0%";
+    state.bar.dataset.rightCellNum = state.start_cell_num;
+    state.bar.dataset.leftCellNum = state.start_cell_num;
+    state.bar.dataset.top = 0;
+    state.bar.style.top = "0%";
 
-    addBarToCell(drag_state);
+    addBarToCell(state);
 
-    drag_state.isCreated = true;
-  } else if (drag_state.isCreated) {
+    state.isCreated = true;
+  } else if (state.isCreated) {
 
-    const num = getCellNum(drag_state, e.clientX);
-    if (num != drag_state.prev_focus_num) {
-      drag_state.prev_focus_num = num;
-      if (num > drag_state.start_cell_num) {
-        drag_state.bar.dataset.rightCellNum = num;
+    const num = getCellNum(state, e.clientX);
+    if (num != state.prev_focus_num) {
+      state.prev_focus_num = num;
+      if (num > state.start_cell_num) {
+        state.bar.dataset.rightCellNum = num;
       } else {
-        drag_state.bar.dataset.leftCellNum = num;
-        drag_state.bar.dataset.rightCellNum = drag_state.start_cell_num;
+        state.bar.dataset.leftCellNum = num;
+        state.bar.dataset.rightCellNum = state.start_cell_num;
       }
 
-      drag_state.bar_holder.removeChild(drag_state.bar);
-      const replace = drag_state.backed_week.cloneNode(true);
-      drag_state.week.replaceWith(replace);
-      drag_state.week = replace;
-      drag_state.cells = replace.getElementsByClassName('day-cell');
-      addBarToCell(drag_state);
+      state.bar_holder.removeChild(state.bar);
+      const replace = state.backed_week.cloneNode(true);
+      state.week.replaceWith(replace);
+      state.week = replace;
+      state.cells = replace.getElementsByClassName('day-cell');
+      addBarToCell(state);
 
-      let right_cell = drag_state.cells[drag_state.bar.dataset.rightCellNum]; 
-      let left_cell = drag_state.cells[drag_state.bar.dataset.leftCellNum]; 
+      let right_cell = state.cells[state.bar.dataset.rightCellNum]; 
+      let left_cell = state.cells[state.bar.dataset.leftCellNum]; 
 
       let newWidth = right_cell.getBoundingClientRect().right-
         left_cell.getBoundingClientRect().left-1;
-      drag_state.bar.style.width = newWidth+'px';
+      state.bar.style.width = newWidth+'px';
     }
   }
 }
-
-document.addEventListener('DOMContentLoaded', (event) => {
-  setMonthScrollPosition();
-  const calendarBody = document.getElementById('calendar-body');
-  calendarBody.addEventListener('mousedown', handleMouseDown);
-  calendarBody.addEventListener('mouseup', handleMouseUp);
-  calendarBody.addEventListener('mousemove', handleMouseMove);
-
-  const sideMenu = document.getElementById('side-menu-container');
-  sideMenu.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-    let menu = document.getElementById('right-click-menu');
-    menu.style.display = 'flex';
-    menu.style.left = e.clientX + 'px';
-    menu.style.top = e.clientY + 'px';
-  });
-});
-
-let handleTyping = null;
-
-document.addEventListener('htmx:afterSwap', (event) => {
-  const url = event.detail.xhr.responseURL;
-  const path = getPath(url);
-
-  if (path === '/' || path === '/view/month') {
-    setMonthScrollPosition();
-  } else if (path === '/api/scrolling-up') {
-    const calendarBody = document.getElementById('calendar-body');
-    const calendarContent = document.getElementById('calendar-content');
-    const week = calendarContent.querySelectorAll('.week-row')[0];
-    const originalScrollBehavior = calendarBody.style.scrollBehavior;
-    calendarBody.style.scrollBehavior = 'auto';
-    calendarBody.scrollTop = previousScroll + 6*week.offsetHeight;
-    console.log("new scroll: ", calendarBody.scrollTop);
-    calendarBody.style.scrollBehavior = originalScrollBehavior;
-  } else if (path === '/api/scrolling-down') {
-    const calendarBody = document.getElementById('calendar-body');
-    const calendarContent = document.getElementById('calendar-content');
-    const week = calendarContent.querySelectorAll('.week-row')[0];
-    const originalScrollBehavior = calendarBody.style.scrollBehavior;
-    calendarBody.style.scrollBehavior = 'auto';
-    calendarBody.scrollTop = previousScroll - 6*week.offsetHeight;
-    console.log("new scroll: ", calendarBody.scrollTop);
-    calendarBody.style.scrollBehavior = originalScrollBehavior;
-  }
-});
-
-document.addEventListener('htmx:beforeSwap', (event) => {
-  const url = event.detail.xhr.responseURL;
-  const path = getPath(url);
-
-  if (path === '/api/scrolling-up' || path === '/api/scrolling-down') {
-    previousScroll = document.getElementById("calendar-body").scrollTop;
-    console.log("prev scroll: ", previousScroll);
-  }
-});
