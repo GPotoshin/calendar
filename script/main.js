c72d4da48abd745dc2bdb7ba8386ba857b48e238
George Potoshin <george.potoshin@gmail.com>
2025-12-03 11:17:29 +0100

    working on ui

--- script/main.js
+++ script/main.js
@@ -41,91 +41,44 @@ function readData(reader) {
 }
 
 let elements = {
-  side_menu: null,
+  sideMenu: null,
+  sideMenuContainer: null,
+  createOptionMenu: null,
+  nameList: null,
+  venueList: null,
+  rightClickMenu: null,
 }
 
-elements.side_menu = document.createElement('div');
-elements.side_menu.classList.add("v-container");
-elements.side_menu.id = 'side-menu';
+elements.sideMenu = document.createElement('div');
+elements.sideMenu.classList.add('v-container');
+elements.sideMenu.id = 'side-menu';
 
-function getPath(u) {
+document.addEventListener('DOMContentLoaded', async (event) => {
+  elements.sideMenuContainer = document.getElementById('side-menu-container');
+  elements.nameList = document.getElementById('name-list');
+  elements.venueList = document.getElementById('venue-list');
+  elements.rightClickMenu = document.getElementById('right-click-menu');
+  elements.createOptionMenu = document.getElementById('create-option-menu');
+
+  setMonthScrollPosition();
+  const calendarBody = document.getElementById('calendar-body');
+  calendarBody.addEventListener('mousedown', handleMouseDown);
+  calendarBody.addEventListener('mouseup', handleMouseUp);
+  calendarBody.addEventListener('mousemove', handleMouseMove);
+
   try {
-    const url_obj = new URL(u);
-    return url_obj.pathname;
-  } catch (e) {
-    console.error("Invalid URL:", u, e);
-    return "";
-  }
-}
-window.getPath = getPath;
-
-function handleClickOnSideMenuButton(button) {
-  let side_menu_container = document.getElementById('side-menu-container');
-
-  if (state.sideMenuIsOpen) {
-    side_menu_container.removeChild(elements.side_menu);
-  } else {
-    side_menu_container.appendChild(elements.side_menu);
+    const response = await fetch('/data');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const bin = await response.arrayBuffer();
+    const r = new BufferReader(bin);
+    data = readData(r)
+  } catch (error) {
+    console.error('Could not fetch data:', error);
   }
 
-  state.sideMenuIsOpen ^= true;
-}
-window.handleClickOnSideMenuButton = handleClickOnSideMenuButton;
-
-async function handleClickForOptionMenu(event) {
-  let menu = document.getElementById('create-option-menu');
-  if (!menu.contains(event.target)) {
-    menu.style.display = 'none';
-    document.removeEventListener('click', handleClickForOptionMenu);
-    let input = menu.querySelectorAll('input')[0];
-    input.removeEventListener('input', state.handleTyping);
-
-    const staffIds = [];
-    const nameList = document.getElementById('name-list');
-    for (const name of nameList.children) {
-      if (name.classList.contains('clicked')) {
-        staffIds.push(parseInt(name.dataset.idx));
-      }
-    }
-
-    const enueIds = [];
-    const venueList = document.getElementById('venue-list');
-    for (const venue of venueList.children) {
-      if (venue.classList.contains('clicked')) {
-        venueIds.push(parseInt(venue.dataset.idx));
-      }
-    }
-
-    const writer = new BufferWriter();
-    writer.writeString(input.value);
-    writer.writeInt32Array(staffIds);
-    writer.writeInt32Array(venueIds);
-    
-    try {
-      const resp = await fetch("store/event", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/octet-stream',
-        },
-        body: writer.getBuffer(),
-      });
-
-      if (!resp.ok) {
-        throw new Error(`HTTP error! status: ${resp.status}`);
-      }
-    } catch(e) {
-      console.error(`Error: ${e}`);
-    }
-
-    input.value = "";
-  }
-};
-
-function handleClickForContextMenu() {
-  let menu = document.getElementById('right-click-menu');
-  menu.style.display = 'none';
-  document.removeEventListener('click', handleClickForContextMenu);
-}
+});
 
 document.addEventListener('click', (event) => {
   if (event.target.id == 'new-event-button') {
@@ -137,35 +90,35 @@ document.addEventListener('click', (event) => {
   }
 });
 
-document.addEventListener('DOMContentLoaded', async (event) => {
-  setMonthScrollPosition();
-  const calendarBody = document.getElementById('calendar-body');
-  calendarBody.addEventListener('mousedown', handleMouseDown);
-  calendarBody.addEventListener('mouseup', handleMouseUp);
-  calendarBody.addEventListener('mousemove', handleMouseMove);
-
-  try {
-    const response = await fetch("/data");
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const bin = await response.arrayBuffer();
-    const r = new BufferReader(bin);
-    data = readData(r)
-  } catch (error) {
-    console.error('Could not fetch data:', error);
-  }
-
-  const sideMenu = document.getElementById('side-menu-container');
-  sideMenu.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-    let menu = document.getElementById('right-click-menu');
-    menu.style.display = 'flex';
-    menu.style.left = e.clientX + 'px';
-    menu.style.top = e.clientY + 'px';
-    document.addEventListener('click', handleClickForContextMenu);
-  });
+document.addEventListener('contextmenu', function(e) {
+  const menu = elements.rightClickMenu;
+  if (elements.sideMenu.contains(e.target)) {
+      e.preventDefault();
+      menu.style.display = 'flex';
+      menu.style.left = e.clientX + 'px';
+      menu.style.top = e.clientY + 'px';
+      document.getElementById('new-event-button').style.display = "block";
+      document.addEventListener('click', handleClickForContextMenu);
+  } else if (elements.nameList.contains(e.target)) {
+      e.preventDefault();
+      menu.style.display = 'flex';
+      menu.style.left = e.clientX + 'px';
+      menu.style.top = e.clientY + 'px';
+      document.getElementById('new-member-button').style.display = "block";
+      document.addEventListener('click', handleClickForContextMenu);
+  } 
 });
+
+function getPath(u) {
+  try {
+    const url_obj = new URL(u);
+    return url_obj.pathname;
+  } catch (e) {
+    console.error("Invalid URL:", u, e);
+    return "";
+  }
+}
+window.getPath = getPath;
 
 document.addEventListener('htmx:afterSwap', (event) => {
   const url = event.detail.xhr.responseURL;
@@ -201,6 +154,106 @@ document.addEventListener('htmx:beforeSwap', (event) => {
   }
 });
 
+function handleClickOnSideMenuButton(button) {
+  let sideMenuContainer = elements.sideMenuContainer;
+
+  if (state.sideMenuIsOpen) {
+    sideMenuContainer.removeChild(elements.sideMenu);
+  } else {
+    sideMenuContainer.appendChild(elements.sideMenu);
+  }
+
+  state.sideMenuIsOpen ^= true;
+}
+window.handleClickOnSideMenuButton = handleClickOnSideMenuButton;
+
+function handleCreateNewStaffMember() {
+  const staffList = elements.nameList;
+  const button = document.createElement('button');
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.id = 'agentname';
+  input.name = 'eventname';
+  input.placeholder = 'Nouvel Agent';
+  button.appendChild(input);
+  staffList.appendChild(button);
+  input.focus();
+
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      const value = input.value;
+      input.remove();
+      button.textContent = value;
+      button.onclick = function() {
+        handle2StateButtonClick(this);
+      };
+      button.className = 'hover';
+    } else if (e.key === 'Escape') {
+      input.remove();
+    }
+  });
+}
+window.handleCreateNewStaffMember = handleCreateNewStaffMember;
+
+async function handleClickForOptionMenu(event) {
+  let menu = elements.createOptionMenu;
+  if (!menu.contains(event.target) && !element.rightClickMenu(event.target)) {
+    menu.style.display = 'none';
+    document.removeEventListener('click', handleClickForOptionMenu);
+    let input = menu.querySelectorAll('input')[0];
+    input.removeEventListener('input', state.handleTyping);
+
+    const staffIds = [];
+    const nameList = elements.nameList;
+    for (const name of nameList.children) {
+      if (name.classList.contains('clicked')) {
+        staffIds.push(parseInt(name.dataset.idx));
+      }
+    }
+
+    const venueIds = [];
+    const venueList = element.venueList;
+    for (const venue of venueList.children) {
+      if (venue.classList.contains('clicked')) {
+        venueIds.push(parseInt(venue.dataset.idx));
+      }
+    }
+
+    const writer = new BufferWriter();
+    writer.writeString(input.value);
+    writer.writeInt32Array(staffIds);
+    writer.writeInt32Array(venueIds);
+    
+    try {
+      const resp = await fetch("store/event", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/octet-stream',
+        },
+        body: writer.getBuffer(),
+      });
+
+      if (!resp.ok) {
+        throw new Error(`HTTP error! status: ${resp.status}`);
+      }
+    } catch(e) {
+      console.error(`Error: ${e}`);
+    }
+
+    input.value = "";
+  }
+};
+
+function handleClickForContextMenu() {
+  let menu = elements.rightClickMenu;
+  menu.style.display = 'none';
+  document.removeEventListener('click', handleClickForContextMenu);
+  for (const child of menu.children) {
+    child.style.display = 'none';
+  }
+}
+
 function handleClickOnEventButton(e) {
   e.style.backgroundColor = palette.red;
   if (state.selected_element) {
@@ -212,7 +265,7 @@ window.handleClickOnEventButton = handleClickOnEventButton;
 
 
 function handleCreateNewEvent() {
-  let menu = document.getElementById("side-menu");
+  let menu = elements.sideMenu;
   let new_button = document.createElement('button');
   new_button.classList.add('event-button');
   new_button.textContent = "Nouvel Événement";
